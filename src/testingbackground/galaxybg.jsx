578c6b0fb9e9a3e88daaa8aed8482df6c5b57ba2
eman13-developer <efatima039695@gmail.com>
2026-03-07 08:29:40 +0500

    update website

--- src/testingbackground/galaxybg.jsx
+++ src/testingbackground/galaxybg.jsx
@@ -1,36 +1,145 @@
-import { Canvas, useFrame, useThree } from "@react-three/fiber";
-import { Stars } from "@react-three/drei";
-import { useRef } from "react";
+// import { Canvas, useFrame, useThree } from "@react-three/fiber";
+// import { Stars } from "@react-three/drei";
+// import { useRef } from "react";
 
-function MovingStars() {
-  const starsRef = useRef();
-  const { mouse } = useThree();
+import { useEffect, useRef } from "react";
 
-  useFrame(() => {
-    if (starsRef.current) {
-      starsRef.current.rotation.x = mouse.y * 0.5;
-      starsRef.current.rotation.y = mouse.x * 0.5;
+export default function Galaxy() {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    let stars = [];
+    let mouse = { x: null, y: null };
+
+    window.addEventListener("mousemove", (e) => {
+      mouse.x = e.x;
+      mouse.y = e.y;
+    });
+
+    class Star {
+      constructor() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 2;
+        this.speedX = (Math.random() - 0.5) * 0.5;
+        this.speedY = (Math.random() - 0.5) * 0.5;
+      }
+
+      update() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+
+        // cursor repulsion
+        if (mouse.x && mouse.y) {
+          let dx = this.x - mouse.x;
+          let dy = this.y - mouse.y;
+          let dist = Math.sqrt(dx * dx + dy * dy);
+
+          if (dist < 120) {
+            this.x += dx / 10;
+            this.y += dy / 10;
+          }
+        }
+
+        // screen wrap
+        if (this.x < 0) this.x = canvas.width;
+        if (this.x > canvas.width) this.x = 0;
+        if (this.y < 0) this.y = canvas.height;
+        if (this.y > canvas.height) this.y = 0;
+      }
+
+      draw() {
+        ctx.fillStyle = "white";
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.fill();
+      }
     }
-  });
+
+    for (let i = 0; i < 400; i++) {
+      stars.push(new Star());
+    }
+
+    function animate() {
+      ctx.fillStyle = "black";
+      ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+      stars.forEach((star) => {
+        star.update();
+        star.draw();
+      });
+
+      requestAnimationFrame(animate);
+    }
+
+    animate();
+  }, []);
 
   return (
-    <group ref={starsRef}>
-      <Stars radius={100} depth={50} count={5000} factor={4} fade speed={1} />
-    </group>
-  );
-}
-
-export default function GalaxyBackground() {
-  return (
-    <Canvas
-      camera={{ position: [0, 0, 1] }}
+    <canvas
+      ref={canvasRef}
       style={{
-        width: "100vw",
-        height: "100vh",
-        background: "black",
+        position: "fixed",
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        zIndex: -1
       }}
-    >
-      <MovingStars />
-    </Canvas>
+    />
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function MovingStars() {
+//   const starsRef = useRef();
+//   const { mouse } = useThree();
+
+//   useFrame(() => {
+//     if (starsRef.current) {
+//       starsRef.current.rotation.x = mouse.y * 0.5;
+//       starsRef.current.rotation.y = mouse.x * 0.5;
+//     }
+//   });
+
+//   return (
+//     <group ref={starsRef}>
+//       <Stars radius={100} depth={50} count={5000} factor={4} fade speed={1} />
+//     </group>
+//   );
+// }
+
+// export default function GalaxyBackground() {
+//   return (
+//     <Canvas
+//       camera={{ position: [0, 0, 1] }}
+//       style={{
+//         width: "100vw",
+//         height: "100vh",
+//         background: "black",
+//       }}
+//     >
+//       <MovingStars />
+//     </Canvas>
+//   );
+// }
--- src/testingbackground/testingbg.jsx
+++ src/testingbackground/testingbg.jsx
@@ -1,9 +1,33 @@
-import GalaxyBackground from "./galaxybg";
-
+import Galaxy from "./galaxybg";
 function Testingbg() {
   return (
     <div>
-      <GalaxyBackground />
+       <Galaxy />
+
+      <h1
+        style={{
+          color: "white",
+          position: "absolute",
+          top: "40%",
+          left: "40%",
+        }}
+      >
+        Hii! I am Eman
+      </h1>
+    </div>
+  );
+}
+
+export default Testingbg;
+
+
+
+
+
+
+
+
+{/* <GalaxyBackground />
 
       <h1
         style={{
@@ -15,8 +39,31 @@ function Testingbg() {
       >
         Galaxy Background
       </h1>
-    </div>
-  );
-}
+    </div> */}
 
-export default Testingbg;
+
+
+
+
+
+
+
+
+
+
+
+
+    // function Testingbg() {
+//   return (
+//      <div className="">
+//       <Galaxy />
+
+//       <div style={{ color: "white", textAlign: "center", marginTop: "200px" }}>
+//         <h1>Galaxy Background</h1>
+//         <p>Move your cursor and see the stars react</p>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Testingbg;
